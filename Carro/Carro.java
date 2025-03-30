c61dd760b4be3f3de0c2e6cf28486101a8a9672a
Gabriel Macedo <123653810+GabrielMacedo07@users.noreply.github.com>
2025-03-29 21:43:58 -0300

    atividades

---
+++ Carro/Carro.java
@@ -0,0 +1,44 @@
+package Carro;
+
+public class Carro {
+	private String modelo;
+	private String marca;
+	private String cor;
+	
+	public Carro(String modelo, String marca, String cor) {
+		this.modelo = modelo;
+		this.marca = marca;
+		this.cor = cor;
+		
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public void exibir() {
+		System.out.println("Marca: "+marca);
+		System.out.println("Modelo: "+modelo);
+		System.out.println("Cor: "+cor);
+	}
+}
---
+++ Carro/Main.java
@@ -0,0 +1,12 @@
+package Carro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Carro c = new Carro("Chevrolet", "Camaro", "amarelo");
+		
+		c.exibir();
+		
+	}
+
+}
